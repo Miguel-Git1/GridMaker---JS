2ec3af720bb4f7bf18aac2cd0cc1688c64c7c003
Miguel Joaquim <miguelmews123vv@gmail.com>
2026-06-07 18:51:55 +0100

    Create a layer manager

---
+++ modules/layers/layerManager.js
@@ -0,0 +1,35 @@
+export class LayerManager {
+    
+    #layerContainer;
+    #layerMapper = new Map();
+
+    constructor() {
+        this.#layerContainer = document.getElementById("layer-container");
+    }
+
+    createLayer(name) {
+        const newCanvas = document.createElement("canvas");
+        newCanvas.id = name;
+        newCanvas.dataset.index = this.#layerMapper.size + 1;
+
+        this.#layerMapper.set(name, newCanvas);
+
+        this.#layerContainer.insertBefore(newCanvas, null);
+
+        return newCanvas;
+    }
+
+    fetchLayerContext(name)
+    {
+        const targetCanvas = this.#layerMapper.get(name);
+
+        return targetCanvas.getContext("2d");
+    }
+
+    upsertAllCanvasSize() {
+        for (const [id, canvas] of this.#layerMapper) {
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+        }
+    }
+}
