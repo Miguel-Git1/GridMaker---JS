db75b6685572633e4ad93d01e02aaf72740291e0
Miguel Joaquim <miguelmews123vv@gmail.com>
2026-06-07 14:34:20 +0100

    Create private methods in the game state to track mouse position.
    We may do this in the new class input mapper. But for now it stays here.

--- GameState.js
+++ GameState.js
@@ -8,6 +8,29 @@ class GameState {
     isMouseDown;
 
     themeColor;
+
+    #hookMouseTrack() {
+        document.addEventListener("mousemove", (e) => {
+            this.mousePosX = e.clientX;
+            this.mousePosY = e.clientY;
+        });
+    }
+
+    #hookMouseInputs() {
+        document.addEventListener("mousedown", () => {
+            this.isMouseDown = true;
+        });
+
+        document.addEventListener("mouseup", () => {
+            this.isMouseDown = false;
+        });
+    }
+
+    constructor() {
+        this.#hookMouseTrack();
+        this.#hookMouseInputs();
+    }
+
 }
 
 
