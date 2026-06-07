4f881913ef1c8e7374d1322ecc96cc82ffcb00c4
Miguel Joaquim <miguelmews123vv@gmail.com>
2026-06-07 18:52:24 +0100

    Export the canvas creation to a seperate class

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 import { grid } from "./modules/grid/grid.js";
 import { gameState } from "./GameState.js";
-import { inputActionManager } from "./modules/Inputs/InputActionManager.js";
+import { inputActionManager } from "./modules/inputs/InputActionManager.js";
+import { LayerManager } from "./modules/layers/layerManager.js"
 
-
-const canvas = document.getElementById("mycanvas");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-const context = canvas.getContext("2d");
 
 const columnSlider = document.getElementById("grid-column-slider");
 const rowSlider = document.getElementById("grid-row-slider");
@@ -15,31 +11,32 @@ const colorPicker = document.getElementById("stroke-color")
 
 colorPicker.addEventListener("input", (e) => {
     gameState.themeColor = e.target.value;
-    console.log(e.target.value);
-})
+});
 
-;requestAnimationFrame(mainGameLoop);
+const layerManager = new LayerManager();
+const gridLayer = layerManager.createLayer("grid-layer");
+const pixelLayer = layerManager.createLayer("pixel-layer");
+
+;requestAnimationFrame(warmUp);
+
+;function warmUp() {
+    requestAnimationFrame(mainGameLoop);
+
+    layerManager.upsertAllCanvasSize();
+    
+    grid.drawGrid(gridLayer.getContext("2d"));
+}
 
 ;function mainGameLoop() {
     requestAnimationFrame(mainGameLoop);
 
-    gameState.innerWidth = window.innerWidth;
-    gameState.innerHeight = window.innerHeight;
-
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    context.reset();
-
-    context.fillStyle = gameState.themeColor;
-
     grid.changeGridDims(columnSlider.valueAsNumber, rowSlider.valueAsNumber);
 
-    grid.drawGrid(context)
+    pixelLayer.getContext("2d").reset();
 
-    grid.drawPositionedSquare(context);
-
-    grid.paintPositionedSquare(context);
+    grid.drawPositionedSquare(pixelLayer.getContext("2d"));
+    
+    grid.paintPositionedSquare(pixelLayer.getContext("2d"));
 
     mousePosLabel.textContent = `X: ${gameState.mousePosX} Y: ${gameState.mousePosY}`;
 
